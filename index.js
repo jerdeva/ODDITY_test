e2269c0d6e9d202c4cd3739ff94430f76c442ad4
Olena Jerdeva <lena.jerdeva28@gmail.com>
2024-07-11 13:52:51 +0300

    preventDefault on searchBtn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ document.getElementById("luckyBtn").addEventListener("click", function (event) {
   event.preventDefault();
   document.getElementById("logo").src = "./img/odditytech_logo.jpeg";
 });
+
+document
+  .getElementById("searchBtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+  });
