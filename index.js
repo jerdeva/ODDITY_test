5c6d81507632e74bcbabd5f9b0a76b6ac3abc73b
Olena Jerdeva <lena.jerdeva28@gmail.com>
2024-07-11 12:58:02 +0300

    page without js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("luckyButton").addEventListener("click", function () {
+  window.location.href =
+    "https://masterpiecer-images.s3.yandex.net/5fd531dca6427c7:upscaled";
+});
